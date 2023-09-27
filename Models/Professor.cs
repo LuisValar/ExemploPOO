081b644d615b84b8e29f357e9abbfcdf08372419
LuisValar <luis.h.valar@gmail.com>
2023-09-27 15:01:52 -0300

    poo dio

---
+++ Models/Professor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public class Professor : Pessoa
+    {
+        public Professor(string nome) : base(nome){ }
+        public decimal Salario { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, e sou um professor com salário: {Salario}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ExemploPOO.Interfaces;
+using ExemploPOO.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(123);
+c1.ExibirSaldo();
+
+Pessoa p1 = new Pessoa("Valar");
+Aluno a1 = new Aluno("Luis");
+a1.Apresentar();
+
+
+Corrente c = new Corrente();
+c.Creditar(500);
+c.ExibirSaldo();
+
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Multiplicar(1, 1));
